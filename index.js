f51951c82008fe967847c4afa70279686f039f24
Manyagupta0612 <105008331+Manyagupta0612@users.noreply.github.com>
2024-05-09 11:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const host = 3000
+const host = 5000
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
